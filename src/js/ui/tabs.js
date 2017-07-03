c4d1f6f644530a732395dd401f7a91861370ba0e
Bob Wallis <bob.wallis@gmail.com>
2017-07-03 22:21:12 +0100

    Set hash on tab change and load page to right initial tab

--- src/js/ui/tabs.js
+++ src/js/ui/tabs.js
@@ -1,15 +1,22 @@
 define( ['jquery'], function( $ ) {
 	var $tabs = $( '#tabs' ),
-    allTabsAndContents = $( '#tabs > li, #contents > div' );
+    	allTabsAndContents = $( '#tabs > li, #contents > div' );
+	
 	$tabs.on( 'click' , 'li', function( e ) {
 		var activeID = e.target.id.replace( /^tab_/, '' );
 		if( activeID === 'github' ) {
 			location.href = 'https://github.com/bobwallis/Standard41';
 		}
 		else {
+			window.location.hash = activeID;
 			allTabsAndContents.removeClass( 'active' );
 			$( '#tab_'+activeID+', #content_'+activeID ).addClass( 'active' );
 		}
 	} );
+
+	if( window.location.hash !== '' ) {
+		$( '#tab_'+window.location.hash.substr(1) ).click();
+	}
+
     return $tabs;
 } );
